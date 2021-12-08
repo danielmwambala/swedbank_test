00b47a98578d96f39c63f742b24459beed2f5261
danielmwambala <danielmwambala@gmail.com>
2021-12-08 12:42:17 +0300

    Initial commit

--- src/components/End.js
+++ src/components/End.js
@@ -8,7 +8,7 @@ const End = () => {
     <div className="section">
       <img src={logo} alt="" />
       <div className="landing__container">
-        <h1>Thank you for your time 🙂 </h1>
+        <h1>Thank you for your Submission 🙂 </h1>
         <div>
           <Link to="/">
             <button className="btn btn__active">Go to homepage</button>
